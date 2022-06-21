a394f4bc46f7e48d9f7228f4044c5f449434fb5e
Ajdavidl <ajdavidl@yahoo.com.br>
2022-06-21 12:20:37 -0300

    Adicionada funcionalidade de traduzir texto.

--- RasaServer/actions/actions.py
+++ RasaServer/actions/actions.py
@@ -25,3 +25,31 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+from datetime import datetime, date, timedelta
+
+from textblob import TextBlob
+
+class translate(Action):
+    def name(self) -> Text:
+        return "action_translate"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        message = tracker.latest_message['text']
+        list_words = message.split(' ')
+        idiomaFrom = list_words[1]
+        idiomaTo = list_words[2]
+        query = ' '.join(list_words[3:])
+
+        blob = TextBlob(query).translate(to=idiomaTo, from_lang = idiomaFrom)
+        string = str(blob)
+
+        dispatcher.utter_message(string)
+        return []
