8f8bb3e08e5580b01b2f9798793300319cf6f336
Ajdavidl <ajdavidl@yahoo.com.br>
2022-06-22 12:27:33 -0300

    Adicionada a funcionalidade de parsing.

--- RasaServer/actions/actions.py
+++ RasaServer/actions/actions.py
@@ -34,6 +34,7 @@ from rasa_sdk.events import UserUtteranceReverted
 from datetime import datetime, date, timedelta
 
 from textblob import TextBlob
+import spacy
 
 class translate(Action):
     def name(self) -> Text:
@@ -52,4 +53,41 @@ class translate(Action):
         string = str(blob)
 
         dispatcher.utter_message(string)
+        return []
+
+
+class parse(Action):
+    def name(self) -> Text:
+        return "action_parse"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        message = tracker.latest_message['text']
+        list_words = message.split(' ')
+        idioma = list_words[1]
+        sentence = ' '.join(list_words[2:])
+
+        if idioma=='pt':
+            nlp = spacy.load("pt_core_news_sm")
+        elif idioma=='en':
+            nlp = spacy.load("en_core_web_sm")
+        elif idioma=='es':
+            nlp = spacy.load("es_core_news_sm")
+        elif idioma=='it':
+            nlp = spacy.load("it_core_news_sm")
+        elif idioma=='fr':
+            nlp = spacy.load("fr_core_news_sm")
+        elif idioma=='de':
+            nlp = spacy.load("de_core_news_sm")
+        else:
+            dispatcher.utter_message("language not found.")
+            return []
+        
+        doc = nlp(sentence)
+        text = "Token → POS → Tag → Dep\n"
+        for token in doc:
+            text += token.text + ' → ' + token.pos_ + ' → ' + token.tag_ + ' → ' + token.dep_ + '\n'
+        
+        dispatcher.utter_message(text)
         return []
